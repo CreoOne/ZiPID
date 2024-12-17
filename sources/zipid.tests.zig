9c15cd9f2e03c6140b567eaf1cfd4c5582a39200
Tomasz Czarnecki <creo.one@gmail.com>
2024-12-17 21:53:27 +0100

    Removed unnecessary fields initialization in tests

--- sources/zipid.tests.zig
+++ sources/zipid.tests.zig
@@ -18,20 +18,20 @@ test "no state leak" {
 }
 
 test "proportional part" {
-    var pid: ziPID = .{ .proportional = 1.0, .integral = 0.0, .derivative = 0.0 };
+    var pid: ziPID = .{ .proportional = 1.0 };
     const actual = pid.advance(1.0, 0.1);
     try std.testing.expectEqual(1.0, actual);
 }
 
 test "integral part" {
-    var pid: ziPID = .{ .proportional = 0.0, .integral = 1.0, .derivative = 0.0 };
+    var pid: ziPID = .{ .integral = 1.0 };
     _ = pid.advance(1.0, 0.1);
     const actual = pid.advance(1.0, 0.1);
     try std.testing.expectEqual(0.2, actual);
 }
 
 test "derivative part" {
-    var pid: ziPID = .{ .proportional = 0.0, .integral = 0.0, .derivative = 1.0 };
+    var pid: ziPID = .{ .derivative = 1.0 };
     const actual = pid.advance(1.0, 0.1);
     try std.testing.expectEqual(10.0, actual);
 }
@@ -43,28 +43,28 @@ test "all parts" {
 }
 
 test "integral upper bound" {
-    var pid: ziPID = .{ .proportional = 0.0, .integral = 1.0, .derivative = 0.0, .integralMax = 1.0 };
+    var pid: ziPID = .{ .integral = 1.0, .integralMax = 1.0 };
     _ = pid.advance(1.0, 1);
     const actual = pid.advance(1.0, 1);
     try std.testing.expectEqual(1.0, actual);
 }
 
 test "integral lower bound" {
-    var pid: ziPID = .{ .proportional = 0.0, .integral = 1.0, .derivative = 0.0, .integralMin = -1.0 };
+    var pid: ziPID = .{ .integral = 1.0, .integralMin = -1.0 };
     _ = pid.advance(-1.0, 1.0);
     const actual = pid.advance(-1.0, 1.0);
     try std.testing.expectEqual(-1.0, actual);
 }
 
 test "integral positive overflow" {
-    var pid: ziPID = .{ .proportional = 0.0, .integral = 1.0, .derivative = 0.0 };
+    var pid: ziPID = .{ .integral = 1.0 };
     _ = pid.advance(std.math.floatMax(f64), 1.0);
     const actual = pid.advance(1.0, 1.0);
     try std.testing.expectEqual(std.math.floatMax(f64), actual);
 }
 
 test "integral negative overflow" {
-    var pid: ziPID = .{ .proportional = 0.0, .integral = 1.0, .derivative = 0.0 };
+    var pid: ziPID = .{ .integral = 1.0 };
     _ = pid.advance(std.math.floatMin(f64), 1.0);
     const actual = pid.advance(-1.0, 1.0);
     try std.testing.expectEqual(std.math.floatMin(f64), actual);
